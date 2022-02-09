6e2c1bc4ce5652c80c81cea6248f60ec1865f96a
vchivu14 <chivu.vlad@yahoo.com>
2022-02-09 18:03:03 +0100

    jinja templates, flask-wtf forms and validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+# all of the above are flask tools for developing
+from forms import RegistrationForm, LoginForm
+import os
 
 app = Flask(__name__)
 # (__name__) is a special variable in python that is just the name of the module
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 posts = [
     {
@@ -31,11 +35,30 @@ def home():
 def about():
     return render_template('about.html', title="About")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+        # home here is the name of the function we want to redirect to
+    return render_template('register.html', title="Registration", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful!', 'danger')
+    return render_template('login.html', title="Login", form=form)
+
 # we need to set an environment variable to the file that we want to be our flask application.
 # (in this case) 
 # $ export FLASK_APP=flaskblog.py
 
 # OR add the next two lines and run the app with python command
-
 # if __name__ == '__main__':
 # 	app.run(debug=True)
